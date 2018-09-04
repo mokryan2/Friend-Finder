e48d966af5bf2e83f8a102b03a68864dce52448c
Cloud Strife <mokryan17@gmail.com>
2018-09-03 22:43:00 -0500

    Added dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,50 @@
 var friendsArray = [
-
-]
+    {
+        name: "Zach Lucas",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi6vsqFtKDdAhUj7IMKHS7rB2QQjRx6BAgBEAU&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkatzen_auge%2F20066742824&psig=AOvVaw2u40K1M4DFV1rJWRum_yq1&ust=1536118091514072",
+        scores: [
+            1,
+            5,
+            5,
+            3,
+            4,
+            4,
+            2,
+            3,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Tony Barba",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.tonybarba.com%2Fwp-content%2Fuploads%2F2015%2F05%2F42dac46a4480580532216db24c04e.jpg&imgrefurl=https%3A%2F%2Fwww.tonybarba.com%2F&docid=q6qoCvUQ5LYTBM&tbnid=_oaG08emHNiiKM%3A&vet=10ahUKEwiSkIXgtKDdAhWDzIMKHU1OBgAQMwg3KAAwAA..i&w=3000&h=2000&bih=740&biw=767&q=tony%20barba&ved=0ahUKEwiSkIXgtKDdAhWDzIMKHU1OBgAQMwg3KAAwAA&iact=mrc&uact=8",
+        scores: [
+            1,
+            5,
+            4,
+            3,
+            3,
+            5,
+            5,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        name: "D. H. Skogen",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fc1.staticflickr.com%2F1%2F211%2F463730381_dd057bf0e6_z.jpg%3Fzz%3D1&imgrefurl=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Felruskie%2F463730381%2F&docid=JQYroIcTbc_leM&tbnid=8uW9BLSx_CsGeM%3A&vet=10ahUKEwjK9pSwtaDdAhXH24MKHY03Dr8QMwg8KAcwBw..i&w=333&h=500&itg=1&bih=740&biw=767&q=D.%20H.%20Skogen&ved=0ahUKEwjK9pSwtaDdAhXH24MKHY03Dr8QMwg8KAcwBw&iact=mrc&uact=8",
+        scores: [
+            4,
+            5,
+            5,
+            5,
+            5,
+            4,
+            5,
+            5,
+            4,
+            3
+        ]
+    }
+];

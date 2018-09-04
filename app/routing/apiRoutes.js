bc8c0f75907f56a74c49fcef43ac54f1f7d3aa4a
Cloud Strife <mokryan17@gmail.com>
2018-09-04 00:58:48 -0500

    Added final paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,25 @@ module.exports = (app) => {
     });
 
     app.post("/api/friends", (req, res) => {
-        var user
-     })
+        var userInput = req.body;
+        var userResponse = userInput.scores;
+        var matchName = "";
+        var matchImg = "";
+        var totalDiff = 10000;
+
+        for (var i = 0; i < friendData.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < userResponse.length; j++) {
+                diff += Math.abs(friendData[i].scores[j] - userResponse[j]);
+            }
+            if (diff < totalDiff) {
+                totalDiff = diff;
+                matchName = friendData.name;
+                matchImg = friendData.photo;
+            }
+        }
+
+        friendData.push(req.body);
+        res.json({ status: `OK`, matchName: matchName, matchImg: matchImg });
+    })
 };

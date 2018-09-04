e30a5101e9305f0a3c7b81cb81e2561dcdd57ba9
Cloud Strife <mokryan17@gmail.com>
2018-09-04 01:56:04 -0500

    Replaced image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         name: "Zach Lucas",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi6vsqFtKDdAhUj7IMKHS7rB2QQjRx6BAgBEAU&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkatzen_auge%2F20066742824&psig=AOvVaw2u40K1M4DFV1rJWRum_yq1&ust=1536118091514072",
+        photo: "https://d3jjg4nf4bbybe.cloudfront.net/u/48963/b49d3023fff890f7ec81946d419e74a9b609369f/photo/3331622-orig.jpg",
         scores: [
             1,
             5,
@@ -17,7 +17,7 @@ var friendsArray = [
     },
     {
         name: "Tony Barba",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.tonybarba.com%2Fwp-content%2Fuploads%2F2015%2F05%2F42dac46a4480580532216db24c04e.jpg&imgrefurl=https%3A%2F%2Fwww.tonybarba.com%2F&docid=q6qoCvUQ5LYTBM&tbnid=_oaG08emHNiiKM%3A&vet=10ahUKEwiSkIXgtKDdAhWDzIMKHU1OBgAQMwg3KAAwAA..i&w=3000&h=2000&bih=740&biw=767&q=tony%20barba&ved=0ahUKEwiSkIXgtKDdAhWDzIMKHU1OBgAQMwg3KAAwAA&iact=mrc&uact=8",
+        photo: "https://www.tonybarba.com/wp-content/uploads/2015/05/tony-vertica.jpg",
         scores: [
             1,
             5,
@@ -33,7 +33,7 @@ var friendsArray = [
     },
     {
         name: "D. H. Skogen",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fc1.staticflickr.com%2F1%2F211%2F463730381_dd057bf0e6_z.jpg%3Fzz%3D1&imgrefurl=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Felruskie%2F463730381%2F&docid=JQYroIcTbc_leM&tbnid=8uW9BLSx_CsGeM%3A&vet=10ahUKEwjK9pSwtaDdAhXH24MKHY03Dr8QMwg8KAcwBw..i&w=333&h=500&itg=1&bih=740&biw=767&q=D.%20H.%20Skogen&ved=0ahUKEwjK9pSwtaDdAhXH24MKHY03Dr8QMwg8KAcwBw&iact=mrc&uact=8",
+        photo: "http://www.terapija.net/fotke/koncert/20060404_204633_10.jpg",
         scores: [
             4,
             5,
